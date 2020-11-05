767ddb86cd7d358cde807538255aab19a34b2c96
FILL ME OUT <FILL ME OUT>
2020-11-05 14:56:06 -0500

    copy VScode keybindings

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 VS_CODE_EXTENSIONS_PATH="../vscode/vs-code-extensions.sh"
 VS_CODE_SETTINGS_PATH="~/Library/Application Support/Code/User/settings.json"
+VS_CODE_KEYBINDINGS_PATH="~/Library/Application Support/Code/User/keybindings.json"
 
 # OSX defaults
 chflags nohidden ~/Library
@@ -46,6 +47,8 @@ popd
 # install extensions bundle
 cat "$VS_CODE_EXTENSIONS_PATH" | xargs -I _ code --install-extension _
 cp ../vscode/settings.json "$VS_CODE_SETTINGS_PATH"
+cp ../vscode/keybindings.json "$VS_CODE_KEYBINDINGS_PATH"
+
 
 # disables lifecycle scripts on npm install to decrease likelihood of malicious activity when a new package is installed
 npm config set ignore-scripts true
