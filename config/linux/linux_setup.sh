1dfcb006b346d535bf6db9ae0199948fc9d2fc4d
Zach Caceres (zach.dev) <hello@zach.dev>
2021-10-03 19:05:53 -0600

    add ghostwriter

--- config/linux/linux_setup.sh
+++ config/linux/linux_setup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-apt install ack nodejs npm htop iftop tldr wget xsel trash-cli zeal ghostwriter
+apt install ack nodejs npm htop iftop tldr wget xsel trash-cli zeal ghostwriter hunspell-en-us
 
 echo 'installing Rust'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
