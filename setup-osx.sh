0cd32bfac995dffd7e2b8859ff57103b8c824aab
zcaceres <hello@zach.dev>
2022-04-11 20:07:50 -0600

    migrate to pnpm and consolidate messy stuff

--- setup-osx.sh
+++ setup-osx.sh
@@ -9,4 +9,4 @@ brew install ansible
 ansible-galaxy collection install community.general
 
 # Run ansible playbook locally
-ansible-playbook -i ./ansible-osx/inventory ./ansible-osx/site.yml -c local
+ansible-playbook -vvv -i ./ansible-osx/inventory ./ansible-osx/site.yml -c local
