0302147067aa97f66cadf49f798ae1176200fc99
Zach Caceres <zcaceres@users.noreply.github.com>
2023-02-16 07:43:40 -0700

    Update install-homebrew.sh

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -2,4 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# add to path
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
